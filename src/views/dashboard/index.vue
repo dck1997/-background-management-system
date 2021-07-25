11547734b4a8ee7d0112ef254561bc79bffaa18d
dck1997 <70933968+dck1997@users.noreply.github.com>
2021-07-26 02:58:48 +0800

    第一次提交

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="background">
+    <div class="dashboard-container">
+      <div class="dashboard-text">WELCOME,{{ name }}!!!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+    color:blue;
+
+  }
+}
+.background {
+    // width: 100%;
+    // height: 100%;
+    // /**宽高100%是为了图片铺满屏幕 */
+    // z-index: -1;
+    // position: absolute;
+    background:url("../../assets/home.jpg");
+    width:100%;			//大小设置为100%
+    height:100%;			//大小设置为100%
+    position:fixed;
+    background-size:100% 100%;
+}
+
+</style>
